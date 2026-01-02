4304389e790d69654f22ae8855fca22b465cf213
taro <iiinumail@yahoo.co.jp>
2026-01-02 12:49:51 +0900

    落ち葉と雪のエフェクト追加

--- js/script.js
+++ js/script.js
@@ -82,3 +82,472 @@ $(window).on("load", function () {
 $(window).scroll(function () {
   TextTypingAnime();
 });
+
+/*===========================================================*/
+/*印象編 5-5 雪が降る*/
+/*===========================================================*/
+/*雪1*/
+particlesJS("pt1", {
+  particles: {
+    number: {
+      value: 150 /*この数値を変更すると雪の数が増減できる*/,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "image" /*形状は画像を指定*/,
+      stroke: {
+        width: 3,
+        color: "#fff",
+      },
+      image: {
+        src: "img/snow.png" /*画像を指定*/,
+        width: 120,
+        height: 120,
+      },
+    },
+    opacity: {
+      value: 0.7,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 20,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 3 /*この数値を小さくするとゆっくりな動きになる*/,
+      direction: "bottom" /*下に向かって落ちる*/,
+      random: true /*動きはランダム*/,
+      straight: false /*動きをとどめない*/,
+      out_mode: "out" /*画面の外に出るように描写*/,
+      bounce: false /*跳ね返りなし*/,
+      attract: {
+        enable: true,
+        rotateX: 300,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+  },
+  retina_detect: true,
+});
+
+/*雪2*/
+particlesJS("pt2", {
+  particles: {
+    number: {
+      value: 150 /*この数値を変更すると雪の数が増減できる*/,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "image" /*形状は画像を指定*/,
+      stroke: {
+        width: 3,
+        color: "#fff",
+      },
+      image: {
+        src: "img/snow.png" /*画像を指定*/,
+        width: 120,
+        height: 120,
+      },
+    },
+    opacity: {
+      value: 0.7,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 20,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 3 /*この数値を小さくするとゆっくりな動きになる*/,
+      direction: "bottom" /*下に向かって落ちる*/,
+      random: true /*動きはランダム*/,
+      straight: false /*動きをとどめない*/,
+      out_mode: "out" /*画面の外に出るように描写*/,
+      bounce: false /*跳ね返りなし*/,
+      attract: {
+        enable: true,
+        rotateX: 300,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+  },
+  retina_detect: true,
+});
+
+/*===========================================================*/
+/*印象編 5-6 落ち葉*/
+/*===========================================================*/
+particlesJS("pt3", {
+  particles: {
+    number: {
+      value: 15 /*この数値を変更すると落ち葉の数が増減できる*/,
+      density: {
+        enable: true,
+        value_area: 1121.6780303333778,
+      },
+    },
+    color: {
+      value: "#fff",
+    },
+    shape: {
+      type: "image" /*形状は画像を指定*/,
+      stroke: {
+        width: 0,
+      },
+      image: {
+        src: "img/flower.png" /*画像を指定*/,
+        width: 190,
+        height: 204,
+      },
+    },
+    opacity: {
+      value: 0.06409588744762158,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 12,
+      random: true /*サイズをランダムに*/,
+      anim: {
+        enable: false,
+        speed: 4,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 7 /*この数値を小さくするとゆっくりな動きになる*/,
+      direction: "bottom-right" /*右下に向かって落ちる*/,
+      random: false /*動きはランダムにしない*/,
+      straight: false /*動きをとどめない*/,
+      out_mode: "out" /*画面の外に出るように描写*/,
+      bounce: false /*跳ね返りなし*/,
+      attract: {
+        enable: false,
+        rotateX: 281.9177489524316,
+        rotateY: 127.670995809726,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+  },
+  retina_detect: false,
+});
+
+/*落ち葉2*/
+particlesJS("pt4", {
+  particles: {
+    number: {
+      value: 30 /*この数値を変更すると落ち葉の数が増減できる*/,
+      density: {
+        enable: true,
+        value_area: 1121.6780303333778,
+      },
+    },
+    color: {
+      value: "#fff",
+    },
+    shape: {
+      type: "image" /*形状は画像を指定*/,
+      stroke: {
+        width: 0,
+      },
+      image: {
+        src: "img/flower.png" /*画像を指定*/,
+        width: 190,
+        height: 204,
+      },
+    },
+    opacity: {
+      value: 0.06409588744762158,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 12,
+      random: true /*サイズをランダムに*/,
+      anim: {
+        enable: false,
+        speed: 4,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 10 /*この数値を小さくするとゆっくりな動きになる*/,
+      direction: "bottom-right" /*右下に向かって落ちる*/,
+      random: false /*動きはランダムにしない*/,
+      straight: false /*動きをとどめない*/,
+      out_mode: "out" /*画面の外に出るように描写*/,
+      bounce: false /*跳ね返りなし*/,
+      attract: {
+        enable: false,
+        rotateX: 281.9177489524316,
+        rotateY: 127.670995809726,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+  },
+  retina_detect: false,
+});
+
+/*落ち葉3*/
+particlesJS("pt5", {
+  particles: {
+    number: {
+      value: 15 /*この数値を変更すると落ち葉の数が増減できる*/,
+      density: {
+        enable: true,
+        value_area: 1121.6780303333778,
+      },
+    },
+    color: {
+      value: "#fff",
+    },
+    shape: {
+      type: "image" /*形状は画像を指定*/,
+      stroke: {
+        width: 0,
+      },
+      image: {
+        src: "img/flower.png" /*画像を指定*/,
+        width: 190,
+        height: 204,
+      },
+    },
+    opacity: {
+      value: 0.06409588744762158,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 12,
+      random: true /*サイズをランダムに*/,
+      anim: {
+        enable: false,
+        speed: 4,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 7 /*この数値を小さくするとゆっくりな動きになる*/,
+      direction: "bottom-right" /*右下に向かって落ちる*/,
+      random: false /*動きはランダムにしない*/,
+      straight: false /*動きをとどめない*/,
+      out_mode: "out" /*画面の外に出るように描写*/,
+      bounce: false /*跳ね返りなし*/,
+      attract: {
+        enable: false,
+        rotateX: 281.9177489524316,
+        rotateY: 127.670995809726,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+  },
+  retina_detect: false,
+});
+
+/*落ち葉4*/
+particlesJS("pt6", {
+  particles: {
+    number: {
+      value: 50 /*この数値を変更すると落ち葉の数が増減できる*/,
+      density: {
+        enable: true,
+        value_area: 1121.6780303333778,
+      },
+    },
+    color: {
+      value: "#fff",
+    },
+    shape: {
+      type: "image" /*形状は画像を指定*/,
+      stroke: {
+        width: 0,
+      },
+      image: {
+        src: "img/flower.png" /*画像を指定*/,
+        width: 190,
+        height: 204,
+      },
+    },
+    opacity: {
+      value: 0.06409588744762158,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 12,
+      random: true /*サイズをランダムに*/,
+      anim: {
+        enable: false,
+        speed: 4,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 12 /*この数値を小さくするとゆっくりな動きになる*/,
+      direction: "top" /*右下に向かって落ちる*/,
+      random: true /*動きはランダムにしない*/,
+      straight: false /*動きをとどめない*/,
+      out_mode: "out" /*画面の外に出るように描写*/,
+      bounce: false /*跳ね返りなし*/,
+      attract: {
+        enable: false,
+        rotateX: 281.9177489524316,
+        rotateY: 127.670995809726,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+  },
+  retina_detect: false,
+});
